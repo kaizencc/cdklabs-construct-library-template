afe4c6e8335189c52916ec78925dd055d8065312
Kaizen Conroy <zen.conroy@gmail.com>
2022-11-29 18:07:18 -0500

    basic premise

--- src/project.ts
+++ src/project.ts
@@ -1,6 +1,7 @@
-import { typescript } from 'projen';
+import { awscdk } from 'projen';
+// import { UpdateSnapshot } from 'projen/lib/javascript';
 
-export interface CdklabsConstructLibraryOptions extends typescript.TypeScriptProjectOptions {
+export interface CdklabsConstructLibraryOptions extends awscdk.AwsCdkConstructLibraryOptions {
 }
 
 /**
@@ -8,9 +9,28 @@ export interface CdklabsConstructLibraryOptions extends typescript.TypeScriptPro
  *
  * @pjid cdklabs-construct-lib
  */
-export class CdklabsConstructLibrary extends typescript.TypeScriptProject {
+export class CdklabsConstructLibrary extends awscdk.AwsCdkConstructLibrary {
   constructor(options: CdklabsConstructLibraryOptions) {
-    super(options);
-    // TODO
+    super({
+      autoApproveUpgrades: true,
+      autoApproveOptions: {
+        allowedUsernames: ['cdklabs-automation'],
+        secret: 'GITHUB_TOKEN',
+      },
+      authorEmail: 'aws-cdk-dev@amazon.com',
+      authorName: 'Amazon Web Services',
+      authorOrganization: true,
+      // this is the value add, imo. this feature was added in 2 months ago to projen
+      // the default is UpdateSnapshot.ALWAYS, which is a testing risk
+      // https://github.com/projen/projen/pull/2129
+      // jestOptions: {
+      //   updateSnapshot: UpdateSnapshot.NEVER,
+      // },
+      ...options,
+    });
+
+    if (!options.name.startsWith('cdk')) {
+      throw new Error(`cdklabs projects must start with cdk prefix, but got ${options.name}`);
+    }
   }
 }
